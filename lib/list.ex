352b317eab467fd8f95a8b429916655e3dc3993e
JP <jp.bonner@bonner.jp>
2020-10-09 00:41:49 -0600

    hello world

---
+++ lib/list.ex
@@ -0,0 +1,100 @@
+defmodule Todo
+  defmodule List do
+    defstruct id_sequence: 1, entries: %{}
+
+    @type todo_list :: %{}
+    @type entry :: %{date: Date, message: charlist}
+
+    ###################################################################
+    # Interface functions
+    ###################################################################
+
+    @spec start :: pid
+    def start do
+      Server.start(List)
+    end
+
+    @spec create(pid, any) :: any
+    def create(pid, entry) do
+      Server.cast(pid, {:create, entry})
+    end
+    @spec update(pid, any, any) :: any
+    def update(pid, id, entry) do
+      Server.cast(pid, {:update, id, entry})
+    end
+    @spec delete(pid, any) :: any
+    def delete(pid, id) do
+      Server.cast(pid, {:delete, id})
+    end
+    @spec entries(pid, any) :: [List]
+    def entries(pid, date) do
+      IO.puts("List received entries()")
+      Server.call(pid, {:entries, date})
+    end
+
+    ###################################################################
+    # Callback functions
+    ###################################################################
+
+    def init, do: %List{}
+
+    @spec handle_cast({:create, map} |
+    {:delete, any} |
+    {:update, any, any}, List) :: atom | %{entries: map}
+    def handle_cast({:create, entry}, todos) do
+      entry = Map.put(entry, :id, todos.id_sequence)
+
+      entries =
+        Map.put(
+          todos.entries,
+          todos.id_sequence,
+          entry
+        )
+
+      %List{
+        todos
+        | entries: entries,
+          id_sequence: todos.id_sequence + 1
+      }
+    end
+
+    def handle_cast({:update, id, update_body}, todos) do
+      case Map.fetch(todos.entries, id) do
+        :error ->
+          todos
+
+        {:ok, existing_entry} ->
+          new_entry =
+            Enum.reduce(
+              update_body,
+              existing_entry,
+              fn {k, v}, acc ->
+                %{acc | k => v}
+              end
+            )
+
+          entries =
+            Map.put(
+              todos.entries,
+              new_entry.id,
+              new_entry
+            )
+
+          %List{todos | entries: entries}
+      end
+    end
+
+    def handle_cast({:delete, id}, todos) do
+      {_, updated} = Map.pop(todos.entries, id)
+      %List{todos | entries: updated}
+    end
+
+    def handle_call({:entries, date}, todos) do
+      IO.puts("List received handle_call()")
+      entries = todos.entries
+      |> Stream.filter(fn {_, entry} -> entry.date == date end)
+      |> Enum.map(fn {_, entry} -> entry end)
+      {entries, todos}
+    end
+  end
+end
